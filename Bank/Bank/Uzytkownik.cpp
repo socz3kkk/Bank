ad67a120f9b124a107bf2df0ad96c5c53b877bb5
guutekk <91336848+guutekk@users.noreply.github.com>
2026-06-05 11:07:54 +0200

    feat(core): dodanie limitu otwartych kont dla uzytkownika

--- Bank/Bank/Uzytkownik.cpp
+++ Bank/Bank/Uzytkownik.cpp
@@ -17,6 +17,12 @@ Uzytkownik::~Uzytkownik() {
 }
 
 void Uzytkownik::otworzKonto(const string& typKonta) {
+    constexpr size_t LIMIT_KONT = 3;
+    if (mojeKonta.size() >= LIMIT_KONT) {
+        cout << "[BLAD] Odmowa! Osiagnieto maksymalny limit kont (" << LIMIT_KONT << ") dla tego profilu.\n";
+        return;
+    }
+
     string przykladowyNumer = "PL123456789";
 
     Rachunek* noweKonto = new Rachunek(przykladowyNumer, 0.0);
