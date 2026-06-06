b7deca1bc754fa846d890f952b354ab0930411e1
tousukke <dawidgargula1@gmail.com>
2026-06-06 22:54:06 +0200

    feat: implementacja klasy BazaDanych i integracja z JSON
    Dodano klasę BazaDanych realizującą zapis i odczyt danych systemu bankowego do pliku JSON. Zintegrowano bibliotekę nlohmann/json oraz zaimplementowano mechanizm serializacji i deserializacji danych użytkowników. Rozwiązanie pozwala na zachowanie danych pomiędzy uruchomieniami aplikacji.

--- Bank/Bank/BazaDanych.cpp
+++ Bank/Bank/BazaDanych.cpp
@@ -1,8 +1,86 @@
 #include "BazaDanych.h"
 #include "Uzytkownik.h"
-#include "Rachunek.h"
 
 #include <fstream>
-#include <nlohmann/json.hpp>
+#include "json.hpp"
 
 using json = nlohmann::json;
+
+BazaDanych::BazaDanych(const string& plik)
+    : nazwaPliku(plik)
+{
+}
+
+void BazaDanych::zapiszDane(
+    const vector<unique_ptr<Uzytkownik>>& uzytkownicy)
+{
+    json dane;
+
+    dane["uzytkownicy"] = json::array();
+
+    for (const auto& uzytkownik : uzytkownicy)
+    {
+        json klient;
+
+        klient["imie"] =
+            uzytkownik->getImie();
+
+        klient["nazwisko"] =
+            uzytkownik->getNazwisko();
+
+        klient["pesel"] =
+            uzytkownik->getPesel();
+
+        klient["haslo"] =
+            uzytkownik->getHaslo();
+
+        dane["uzytkownicy"].push_back(
+            klient);
+    }
+
+    ofstream plik(nazwaPliku);
+
+    if (!plik.is_open())
+    {
+        return;
+    }
+
+    plik << dane.dump(4);
+
+    plik.close();
+}
+
+void BazaDanych::wczytajDane(
+    vector<unique_ptr<Uzytkownik>>& uzytkownicy)
+{
+    ifstream plik(nazwaPliku);
+
+    if (!plik.is_open())
+    {
+        return;
+    }
+
+    json dane;
+
+    plik >> dane;
+
+    if (!dane.contains("uzytkownicy"))
+    {
+        return;
+    }
+
+    for (const auto& klient : dane["uzytkownicy"])
+    {
+        auto nowyUzytkownik =
+            make_unique<Uzytkownik>(
+                klient["imie"],
+                klient["nazwisko"],
+                klient["pesel"],
+                klient["haslo"]);
+
+        uzytkownicy.push_back(
+            move(nowyUzytkownik));
+    }
+
+    plik.close();
+}
